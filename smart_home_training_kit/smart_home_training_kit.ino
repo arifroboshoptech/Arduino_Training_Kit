8c39d1eea847580630e374a6849759f5f282601c
Muhammad Arif Ridhwan Bin Mohamed Jalil <74816249+arifroboshoptech@users.noreply.github.com>
2023-10-03 22:54:36 +0800

    Update smart_home_training_kit.ino
    Humidity to Hum

--- smart_home_training_kit/smart_home_training_kit.ino
+++ smart_home_training_kit/smart_home_training_kit.ino
@@ -281,7 +281,7 @@ void loop() {
    lcd.setCursor(0,0);
    lcd.print("Temp : " + String(t)+"C");
    lcd.setCursor(0,1);
-   lcd.print("Humidity : " + String(h)+"%");
+   lcd.print("Hum : " + String(h)+"%");
   
   
    read_rfid();
